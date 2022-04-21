497453edb34343735ad1908b9df660da8fb2476a
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-04-21 19:51:19 +0500

    Update editor

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,7 @@
 class RegistrationsController < Devise::RegistrationsController
-  # protected
+  protected
 
-  # def update_resource(resource, params)
-  #   resource.update_without_password(params)
-  # end
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
 end
